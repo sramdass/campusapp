58e1ffeeae96d5fcf74a212751d0943d2e52062d
Satheesh Ramdass <satheesh.ramdass@gmail.com>
2011-12-21 11:42:30 +0530

    Initial Commit. 1. Created a default rails 3.1.1 application. 2. Added the testing gems and frameworks (rspec, watchr and spork) 3. Modified the gitignore file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Campusapp::Application.config.secret_token = '285ec213536da8f10f0b5d30c63c52860073a53a77cfba653f69ff794451ead3e00a3e6e9d23058abf26b5aecaa407efbe39729586672fcfabd4488664c9618b'
