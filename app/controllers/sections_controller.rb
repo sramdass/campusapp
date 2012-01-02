f657cc77dc32d3a3b384cc2e112b9f43b3423754
Satheesh Ramdass <satheesh.ramdass@gmail.com>
2012-01-02 10:19:57 +0530

    Ajaxified the students assignment to section part. Created a new partial for the flashes so that they can be rendered again during the ajax requests.

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -108,32 +108,67 @@ class SectionsController < ApplicationController
   	@section=Section.find(params[:id])
   	@selected_student_id = params[:selected_student_id]
   	@deleted_student_id = params[:deleted_student_id]
-  	#Make sure when a student is clicked more than once, it does not create any duplicate entries.
-  	if @selected_student_id 
-  	  if !SecStudentMap.for_student(@selected_student_id ).for_section(@section.id).all.empty? 
-  	  	flash[:error] = "Student is already present in this section"
+  	
+  	#In non-ajax mode, after adding a student, if the same url is refreshed again -  we will still have
+  	#these parameters. So we will get the flash error messages for duplication. Reset these parameters
+  	params[:selected_student_id] = params[:deleted_student_id] = nil
+  	
+  	
+  	if @selected_student_id   #when a student is selected into this section
+  	  #Make sure when a student is clicked more than once, it does not create any duplicate entries.
+  	  if duplicate_student(@selected_student_id, @section.id)
+  	  	flash.now[:error] = "#{Student.find(@selected_student_id).name} is already present in this section"
   	  	return
   	  end
+  	  #Make sure that the student is not present in other sections for the same year
+  	  if sec = present_in_other_section(@selected_student_id, @section)
+  	  	flash.now[:error] = "#{Student.find(@selected_student_id).name} is already present in another section #{sec.name} - #{sec.clazz.name}"
+  	  	return
+  	  end
+  	  #Assign the selected student to this section
   	  sec_student_map = @section.sec_student_maps.new(:student_id => @selected_student_id)  
-  	  if sec_student_map.save
-  	    respond_to do |format|
-          format.html # assign_students.html.erb
-          format.js
-        end
-      else
-        flash[:error] = "Cannot assign the selected student to this section"
+  	  if !sec_student_map.save
+        flash.now[:error] = "Cannot assign the #{Student.find(@selected_student_id).name} to this section"
       end
-    elsif @deleted_student_id
+    elsif @deleted_student_id #When a student is removed from this section
       #This will return only one entry as an array. Convert the array into a single element
       #Also, Make sure the same student id not called for delete twice (just by url, not by clicking) by checking for nil
       if del = SecStudentMap.for_student(@deleted_student_id ).for_section(@section.id).all.first  
       	del.delete
+      	flash.now[:notice] = "Removed #{Student.find(@deleted_student_id).name} from this section"
       end
     end
+    respond_to do |format|
+      format.html # assign_students.html.erb
+      format.js
+    end      
   end
   
   def studentnew
   	@section = Section.find(params[:id])
   end
   			
+  def duplicate_student(student_id, section_id)
+    if SecStudentMap.for_student(student_id ).for_section(section_id).all.empty?
+      return false
+    else
+	  return true
+end
+  end
+  
+  def present_in_other_section(student_id, section)
+  	maps = SecStudentMap.for_student(student_id ).all
+  	if maps.empty?
+  	  return nil
+  	else
+  	  maps.each do |map|
+  	  	if map.section != section && map.section.clazz.year = section.clazz.year
+  	  	  return map.section
+  	  	end
+  	  end
+  	  return nil
+  	end
+  end
+  
+  
 end
